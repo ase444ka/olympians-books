d77153e1b1d6b52351806d0ab13bdfedc49d7048
Ase444ka <istomina.asia@yandex.ru>
2025-12-13 19:40:41 +0300

    add responsivity to header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -24,10 +24,33 @@ import IconExpand from '@/icons/IconExpand.vue'
   height: 940px;
   background-repeat: no-repeat;
   padding-top: 13px;
+
+  @media screen and (max-width: 1024px) {
+    padding-top: 0px;
+    height: 783px;
+  }
+  @media screen and (max-width: 400px) {
+    padding-top: 7px;
+    height: 590px;
+  }
   .container {
     background-image: url('/images/apollo.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center bottom;
     height: 100%;
     position: relative;
+    @media screen and (max-width: 1024px) {
+      background-size: 150% auto;
+      background-position: center top;
+    }
+    @media screen and (max-width: 768px) {
+      background-position: center bottom;
+    }
+    @media screen and (max-width: 400px) {
+      background-size: 260% auto;
+      background-position: -286px 18px;
+    }
   }
 
   * {
@@ -40,27 +63,76 @@ import IconExpand from '@/icons/IconExpand.vue'
     text-indent: 5px;
     width: 200px;
     letter-spacing: 0px;
+    @media screen and (max-width: 1024px) {
+      font-size: 14px;
+      width: 60px;
+      line-height: 1.8;
+      letter-spacing: 1px;
+    }
+    @media screen and (max-width: 400px) {
+      font-size: 10px;
+      width: 40px;
+    }
   }
 
   h1 {
     width: 60%;
-    margin-top: 84px;
     line-height: 1.5;
     letter-spacing: 6px;
+    @media screen and (max-width: 1024px) {
+      font-size: 64px;
+      width: 90%;
+      letter-spacing: 5px;
+      margin-top: 28px;
+    }
+    @media screen and (max-width: 710px) {
+      width: 75%;
+    }
+    @media screen and (max-width: 400px) {
+      font-size: 40px;
+      letter-spacing: 3px;
+      margin-top: 11px;
+    }
   }
 
   .header-button {
     margin-top: 71px;
+    @media screen and (max-width: 806px) {
+      margin-top: 0;
+    }
+    @media screen and (max-width: 768px) {
+      margin-top: 28px;
+    }
+    @media screen and (max-width: 700px) {
+      margin-top: 0;
+    }
+    @media screen and (max-width: 400px) {
+      margin-top: 5px;
+    }
   }
   .expand {
     position: absolute;
     top: 9px;
     left: calc(50% - 32px);
+    @media screen and (max-width: 1024px) {
+      top: 14px;
+    }
+    @media screen and (max-width: 400px) {
+      top: 6px;
+    }
   }
   .cart {
     position: absolute;
     top: 7px;
     right: 74px;
+    @media screen and (max-width: 1024px) {
+      top: -8px;
+      right: 22px;
+    }
+    @media screen and (max-width: 400px) {
+      top: -15px;
+      right: 8px;
+    }
   }
 }
 </style>
