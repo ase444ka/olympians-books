704a47e471f96b93c9706f4911ea860473d9f6e4
Ase444ka <istomina.asia@yandex.ru>
2025-12-13 14:05:11 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <h1>Книги, которые вознесут вас на Олимп</h1>
+  <p class="main-text">
+    покупайте наши книги
+  </p>
+</template>
+
+<style scoped></style>
