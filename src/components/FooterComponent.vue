05323212fd2a0c427e433c26a54a82e83fc942fc
Ase444ka <istomina.asia@yandex.ru>
2025-12-16 00:07:17 +0300

    add footer adaptivity

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="wrapper">
         <div class="logo">olympians books</div>
-        <a href="tel:+30922040281">+30922040281</a>
+        <a class="phone" href="tel:+30922040281">+30922040281</a>
         <nav>
           <a>Главная</a>
           <a>Каталог</a>
@@ -21,28 +21,64 @@ footer {
   background-color: var(--blue);
   color: var(--white);
   margin-top: 135px;
+  @media screen and (max-width: 960px) {
+    margin-top: 90px;
+  }
   .wrapper {
     display: flex;
     padding: 21px;
     align-items: center;
     justify-content: space-between;
+    @media screen and (max-width: 960px) {
+      padding: 26px 0 30px;
+    }
+    @media screen and (max-width: 660px) {
+display: grid;
+grid-template-columns: 1fr 1fr;
+grid-template-areas:
+                'logo  nav'
+                'phone nav';
+}
   }
   .logo {
     font-family: Archeologicaps;
     font-size: 29px;
     line-height: 45px;
     text-indent: 1px;
-    width: 241px;
+    flex-basis: 241px;
     letter-spacing: 2.8px;
+    grid-area: logo;
+    @media screen and (max-width: 960px) {
+      font-size: 20px;
+      line-height: 32px;
+      flex-basis: 155px;
+      letter-spacing: 1.8px;
+    }
+  }
+  .phone {
+    grid-area: phone;
+    @media screen and (max-width: 660px) {
+text-align: start;
+}
   }
   nav {
     display: flex;
-    gap: 150px;
+    flex-grow: 2;
+    justify-content: space-between;
+    grid-area: nav;
+    @media screen and (max-width: 660px) {
+flex-direction: column;
+}
   }
 
   a {
     font-size: 26px;
     letter-spacing: 1px;
+    flex-grow: 1;
+    text-align: center;
+    @media screen and (max-width: 960px) {
+      font-size: 19px;
+    }
   }
 }
 </style>
